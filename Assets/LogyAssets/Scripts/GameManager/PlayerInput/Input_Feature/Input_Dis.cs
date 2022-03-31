d6e95282a7f52d2397f967f200f310f61712b5d5
jiajin0329 <98139948+jiajin0329@users.noreply.github.com>
2022-03-31 23:05:17 +0800

    input_dis add differencePos

--- Assets/LogyAssets/Scripts/GameManager/PlayerInput/Input_Feature/Input_Dis.cs
+++ Assets/LogyAssets/Scripts/GameManager/PlayerInput/Input_Feature/Input_Dis.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 [System.Serializable]
 public class Input_Dis {
+    static private Vector2  static_differencePos;
+    static public Vector2 differencePos {get{return static_differencePos;}}
+    [SerializeField] private Vector2 _differencePos;
+
+
     static private float  static_dis;
     static public float dis {get{return static_dis;}}
     [SerializeField] private float _dis;
 
     private void _Input_Dis() {
-        static_dis = Vector2.Distance(Logy.Input.touchStartPosition, Logy.Input.touchPosition);
+        static_differencePos = Logy.Input.touchPosition - Logy.Input.touchStartPosition;
+        static_dis = Vector2.Distance(static_differencePos, Vector2.zero);
     }
 
     private void ShowVariable() {
+        _differencePos = static_differencePos;
         _dis = static_dis;;
     }
 
